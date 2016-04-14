fc7e7b469bab66c0ce92ea52f332678162f867a0
Jun Wang <junwang@Juns-MacBook-Air-2.local>
2016-04-13 23:25:27 -0400

    Fix the bug
    Find the bug in the multinomial naive bayes model. Fix the low accuracy
    of prediction.

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,4 +1,3 @@
-
 import nltk
 import nltk.corpus
 from random import shuffle
@@ -43,46 +42,37 @@ class PolaritySents:
                 continue
         return
 
-    def get_pos_sentence(self):
-        return list(self.posSents)
-
-    def size_pos_sentence(self):
-        return len(self.posSents)
-
-    def get_neg_sentence(self):
-        return list(self.negSents)
-
-    def size_neg_sentence(self):
-        return len(self.negSents)
-
-
-
 """
-Divide the dataset
-We need to make sure that the number of positive sentences is equal to that of the negative sentences.
+Preprocess the dataset:
+Divde the dataset into training set and test set
+Find the best feature from the vocabulary of the dataset
+label for positive sentence: 1
+label for negative sentence: 0
 """
-class ClassificationData:
+class Preprocess_Data:
     def __init__(self):
-        self.training_input = []
+        self.training_input =[]
         self.training_output = []
         self.test_input = []
         self.test_output = []
+        self.word_features = set()
 
     def extract_sentence(self, polarity_sents):
         # Get the minimum value of the size
-        pos_size = polarity_sents.size_pos_sentence()
-        neg_size = polarity_sents.size_neg_sentence()
+        pos_size = len(polarity_sents.posSents)
+        neg_size = len(polarity_sents.negSents)
         min_size = min(pos_size, neg_size)
         # shuffle the dataset
-        pos_sentence = polarity_sents.get_pos_sentence()
+        pos_sentence = polarity_sents.posSents
         shuffle(pos_sentence)
-        neg_sentence = polarity_sents.get_pos_sentence()
+        neg_sentence = polarity_sents.negSents
         shuffle(neg_sentence)
 
         pos_sentence = pos_sentence[:min_size]
         neg_sentence = neg_sentence[:min_size]
         return (pos_sentence, neg_sentence)
 
+    # Divide the dataset into training set and test set by cross validation method
     # Divide the dataset into training set and test set by cross validation method
     def divide_dataset(self, fold_index, fold_num, polarity_sents):
         (pos_sentence, neg_sentence) = self.extract_sentence(polarity_sents)
@@ -104,23 +94,18 @@ class ClassificationData:
         self.test_output = [1] * (end_index - start_index) + [0] * (end_index - start_index)
         return None
 
-    def size_training_set(self):
-        return len(self.training_input)
+    def find_best_features(self, original_dataset, num_features = None):
+        if num_features == None:
+            for sentence in original_dataset.posSents:
+                for word in sentence:
+                    self.word_features.add(word)
+            for sentence in original_dataset.negSents:
+                for word in sentence:
+                    self.word_features.add(word)
+        return None
 
-    def size_test_set(self):
-        return len(self.test_input)
 
-    def get_training_input(self):
-        return list(self.training_input)
 
-    def get_training_output(self):
-        return list(self.training_output)
-
-    def get_test_input(self):
-        return list(self.test_input)
-
-    def get_test_output(self):
-        return list(self.test_output)
 
 
 """
@@ -128,15 +113,17 @@ Training the dataset
 """
 # Multinomial naive bayes model
 class Multinomial_NaiveBayesModel:
-    prior = defaultdict(float)
-    cond_prob = defaultdict(float)
+    prior = defaultdict(float) #prior probaility p(c) = count(c) / total_count
+    cond_prob = defaultdict(float) # conditional probability: p(word|c) = (count(c, word) + 1) / (count(c) + |v|)
     vocabulary = set()
     total_word_count = 0.0
-    label_word_count = defaultdict(float) # key: label, value:  number of words
-    tuple_word_count = defaultdict(float) # key: (label, word), value: number of words
+    label_word_count = defaultdict(float) # count(c) key: label, value:  number of words
+    tuple_word_count = defaultdict(float) # count(c, word) key: (label, word), value: number of words
+    theta = 0.01
 
     def __init__(self, dataset = None):
         if dataset != None:
+            self.vocabulary = dataset.word_features
             self.train(dataset.training_input, dataset.training_output)
 
     def train(self, training_input, training_output):
@@ -145,7 +132,8 @@ class Multinomial_NaiveBayesModel:
             label = training_output[i]
             sentence = training_input[i]
             for word in sentence:
-                self.vocabulary.add(word)
+                if word not in self.vocabulary:
+                    continue
                 self.total_word_count += 1.0
                 self.label_word_count[label] += 1.0
                 item = (label, word)
@@ -174,7 +162,6 @@ class Multinomial_NaiveBayesModel:
 
     def classify(self, input):
         pred_output = []
-        label_set = set(self.label_word_count.keys())
         for sentence in input:
             max_prob = 0.0
             pred_label = 0
@@ -187,9 +174,7 @@ class Multinomial_NaiveBayesModel:
                     tmp_prob = self.cond_prob[item]
                     pred_prob += tmp_prob
                 pred_prob = exp(pred_prob)
-                print label, pred_prob, max_prob
                 if pred_prob > max_prob:
-                    print "reset the label"
                     max_prob = pred_prob
                     pred_label = label
             pred_output.append(pred_label)
@@ -207,7 +192,6 @@ class Multinomial_NaiveBayesModel:
         accuracy = correct / total
         return accuracy
 
-"""
 # Bernoulli Naive bayes model
 class Bernoulli_NaiveBayesModel:
     vocabulary = set()  # set of words in the training set
@@ -270,22 +254,19 @@ class Bernoulli_NaiveBayesModel:
 
 
     def test(self, test_input, test_output):
-"""
+
+
+
 
 polarityData = PolaritySents()
 dataset = nltk.corpus.product_reviews_2
 polarityData.preprocess_dataset(dataset)
 dataset = nltk.corpus.product_reviews_1
 polarityData.preprocess_dataset(dataset)
-Preprocessed_dataset = ClassificationData()
+Preprocessed_dataset = Preprocess_Data()
 Preprocessed_dataset.divide_dataset(0, 10, polarityData)
+Preprocessed_dataset.find_best_features(polarityData)
 
 multinomial_naive_bayes_model = Multinomial_NaiveBayesModel(Preprocessed_dataset)
-multinomial_accuracy = multinomial_naive_bayes_model.test(Preprocessed_dataset.get_test_input(), Preprocessed_dataset.get_test_output())
+multinomial_accuracy = multinomial_naive_bayes_model.test(Preprocessed_dataset.test_input, Preprocessed_dataset.test_output)
 print multinomial_accuracy
-
-"""
-bernoulli_naive_bayes_model = Bernoulli_NaiveBayesModel(Preprocessed_dataset)
-bernoulli_accuracy = bernoulli_naive_bayes_model.test(Preprocessed_dataset.get_test_input(), Preprocessed_dataset.get_test_output())
-print bernoulli_accuracy
-"""
